38f0173244be30fe11a34d47da9c87c9b95eb60c
Daniel Garcia <dgarcia@tokenone.com.br>
2025-10-14 20:27:02 -0300

    Correção Bootstrap 2

--- components/BootstrapClient.tsx
+++ components/BootstrapClient.tsx
@@ -5,6 +5,8 @@ import { useEffect } from 'react';
 export default function BootstrapClient() {
   useEffect(() => {
     // Carrega JS de componentes (collapse, dropdown, etc.)
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore - Bootstrap JS não possui tipos oficiais
     import('bootstrap/dist/js/bootstrap.bundle.min.js');
   }, []);
   return null;
